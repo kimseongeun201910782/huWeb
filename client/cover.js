19557935441b9f4f2e55d0b261780662626113bd
kimseongeun1212 <62641359+kimseongeun1212@users.noreply.github.com>
2020-12-30 20:14:57 +0900

    kokokoko

---
+++ client/cover.js
@@ -0,0 +1,16 @@
+FlowRouter.template('/', 'cover');
+
+Template.cover.onRendered(function() {
+    // 화면이 그려지고 난 후 제일 먼저 수행
+    Session.set('count', 0);
+});
+
+Template.cover.helpers({
+    // 화면에 데이터를 전달
+    count: function() {
+        var count = Session.get('count');
+        if (!count) {
+            return '편집하기';
+        }
+    }
+});
---
+++ client/main.js
@@ -0,0 +1,17 @@
+FlowRouter.template('/', 'main');
+
+Template.main.onRendered(function() {
+  // 화면이 그려지고 난 후 제일 먼저 수행
+  Session.set('count', 0);
+});
+
+Template.main.helpers({
+  // 화면에 데이터를 전달
+  count: function() {
+    var count = Session.get('count');
+    if (!count) {
+      return '편집하기';
+    }
+  }
+});
+
