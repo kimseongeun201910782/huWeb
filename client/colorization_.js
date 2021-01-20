02af5abd808d927fe692f2a6ddf972b2234d80ce
mok010 <blueangle1004@gmail.com>
2021-01-21 00:11:23 +0900

    '메모내용'

--- client/colorization_.js
+++ client/colorization_.js
@@ -1,6 +1,69 @@
 FlowRouter.template('/colorization_', 'colorization_');
 
-Template.mosaic_.onRendered(function() {
+Template.colorization_.onRendered(function() {
+
+
+    // Filters
+    function brightnessFilter(pixels, value) {
+        var d = pixels.data;
+        for(var i =0; i< d.length; i+=4){
+            d[i] += value/1.3;
+            d[i+1] += value/1.3;
+            d[i+2] += value/1.3;
+        }
+        return pixels;
+    }
+
+
+
+    function drawImageData(image) {
+        image.height *= canvas.offsetWidth / image.width;
+        image.width = canvas.offsetWidth;
+
+        if (image.height > canvas.offsetHeight) {
+            image.width *= canvas.offsetHeight / image.height;
+            image.height = canvas.offsetHeight;
+        }
+
+        ctx.drawImage(image, 0, 0, image.width, image.height);
+        console.log(ctx.getImageData(0, 0, canvas.width, canvas.height));
+    }
+
+
+    var canvas = $('#canvas')[0];
+    var ctx = canvas.getContext('2d');
+
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    console.log(ctx.getImageData(0, 0, canvas.width, canvas.height));
+
+
+    //var file = image.target.files[0];
+    //var fileReader = new FileReader();
+
+
+    //fileReader.readAsDataURL(file);
+
+    // imageData를 가져온다.
+    var pixels = ctx.getImageData(0,0, canvas.width, canvas.height);
+
+// image processing
+    var filteredData = brightnessFilter(pixels, 100);
+
+// Canvas에 다시 그린다.
+    ctx.putImageData(filteredData, 0 , 0);
+
+   //ctx.drawImage(filteredData, 0, 0, canvas.width, canvas.height);
+
+    //preview.appendChild(filteredData);
+
+    //preview.appendChild(image);  (<업데이트할 데이터선택>,<데이터를 입력>)
+
+    $("#btn-save-colorization").click(function(){ downloadCanvas(this, 'canvas', 'photo')});
+    function downloadCanvas(link, canvasId, filename) {
+        link.href = document.getElementById(canvasId).toDataURL();
+        link.download = filename;
+    }
+
+
 
 });
-
