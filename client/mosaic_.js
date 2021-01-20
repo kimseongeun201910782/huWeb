6400e63799b3b64ab6b95d3bc24ff728a60efa1c
youngseo0526 <60006301+youngseo0526@users.noreply.github.com>
2021-01-20 20:25:02 +0900

    20210120

--- client/mosaic_.js
+++ client/mosaic_.js
@@ -1,6 +1,16 @@
 FlowRouter.template('/mosaic_', 'mosaic_');
 
-Template.mosaic_.onRendered(function() {
-
+Template.mosaic_.onCreated(function() {
+    var _id = FlowRouter.getParam('_id')
 });
 
+Template.mosaic_.helpers({
+    board: function() {
+        var _id = FlowRouter.getParam('_id')
+        return DB_MOSAIC.findOne({_id:_id});
+    },
+    link: function() {
+        // 저장 된 이미지 링크를 반환
+        return DB_FILES.findOne({_id: this.mosaic_id}).link();
+    }
+});
