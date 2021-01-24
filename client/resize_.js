3186183e1385e1e1e0bc5df6dc06ad20fa4e8876
yoon0223 <hjyoon0223@gmail.com>
2021-01-24 21:15:46 +0900

    resize 기능 구현

--- client/resize_.js
+++ client/resize_.js
@@ -1,5 +1,104 @@
 FlowRouter.template('/resize_', 'resize_');
 
 Template.resize_.onRendered(function() {
+    function ResizeImage(ctx, image) {
+        var filesToUpload = document.getElementById('inp-file').files;
+        var file = filesToUpload[0];
 
+        var img = document.createElement("img");
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            img.src = e.target.result;
+            var canvas = document.createElement("canvas");
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+
+            // 최대폭을 400 으로 정했다고 가정했을때
+            // 최대폭을 넘어가는 경우 canvas 크기를 변경해 줍니다.
+            var MAX_WIDTH = 400;
+            var MAX_HEIGHT = 400;
+            var width = img.width;
+            var height = img.height;
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+            canvas.width = width;
+            canvas.height = height;
+
+            //변경된 크기의 이미지
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0, width, height);
+            var dataurl = canvas.toDataURL("image/png");
+            document.getElementById('output').src = dataurl;
+        }
+        reader.readAsDataURL(file);
+    }
+
+    let canvas = $('#canvas')[0];
+    let ctx = canvas.getContext('2d');
+
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    console.log(ctx.getImageData(0, 0, canvas.width, canvas.height));
+
+    /*
+    let pixels = ctx.getImageData(0,0, canvas.width, canvas.height);
+
+    let filteredData = ResizeImage(ctx, pixels);
+    ctx.putImageData(filteredData, 0 , 0); */
 });
+
+/* 내부 스크립트 버전 코드 원문
+<script language = "javascript">
+    function ResizeImage() {
+        var filesToUpload = document.getElementById('inp-file').files;
+        var file = filesToUpload[0];
+
+        var img = document.crea teElement("img");
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            img.src = e.target.result;
+            var canvas = document.createElement("canvas");
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+            // 최대폭을 400 으로 정했다고 가정했을때
+            // 최대폭을 넘어가는 경우 canvas 크기를 변경해 줍니다.
+            var MAX_WIDTH = 400;
+            var MAX_HEIGHT = 400;
+            var width = img.width;
+            var height = img.height;
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+            canvas.width = width;
+            canvas.height = height;
+
+            //변경된 크기의 이미지
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0, width, height);
+            var dataurl = canvas.toDataURL("image/png");
+            document.getElementById('output').src = dataurl;
+        }
+        reader.readAsDataURL(file);
+    }
+</script>
+*/
