a5833ce5adb64c9ceee98c3d88e1e1a39151b986
kimseongeun1212 <62641359+kimseongeun1212@users.noreply.github.com>
2021-01-26 22:32:09 +0900

    resize입력값수정

--- client/resize_.js
+++ client/resize_.js
@@ -1,7 +1,8 @@
 FlowRouter.template('/resize_', 'resize_');
 
 Template.resize_.onRendered(function() {
-    var [a, b] = prompt("두 숫자를 입력하세요: ").split(" ")
+    var a = prompt("설정할 가로 길이를 입력해주세요 (예: 300)").split(" ");
+    var b= prompt(" 설정할 세로 길이를 입력해주세요 (예: 300)").split(" ");
     console.log(a,b);
 
     let canvas = $('#canvas')[0];
@@ -21,55 +22,4 @@ Template.resize_.onRendered(function() {
         link.download = filename;
     }
 
-    /*
-    let pixels = ctx.getImageData(0,0, canvas.width, canvas.height);
-
-    let filteredData = ResizeImage(ctx, pixels);
-    ctx.putImageData(filteredData, 0 , 0); */
 });
-
-/* 내부 스크립트 버전 코드 원문
-<script language = "javascript">
-    function ResizeImage() {
-        var filesToUpload = document.getElementById('inp-file').files;
-        var file = filesToUpload[0];
-
-        var img = document.crea teElement("img");
-
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            img.src = e.target.result;
-            var canvas = document.createElement("canvas");
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0);
-            // 최대폭을 400 으로 정했다고 가정했을때
-            // 최대폭을 넘어가는 경우 canvas 크기를 변경해 줍니다.
-            var MAX_WIDTH = 400;
-            var MAX_HEIGHT = 400;
-            var width = img.width;
-            var height = img.height;
-
-            if (width > height) {
-                if (width > MAX_WIDTH) {
-                    height *= MAX_WIDTH / width;
-                    width = MAX_WIDTH;
-                }
-            } else {
-                if (height > MAX_HEIGHT) {
-                    width *= MAX_HEIGHT / height;
-                    height = MAX_HEIGHT;
-                }
-            }
-            canvas.width = width;
-            canvas.height = height;
-
-            //변경된 크기의 이미지
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0, width, height);
-            var dataurl = canvas.toDataURL("image/png");
-            document.getElementById('output').src = dataurl;
-        }
-        reader.readAsDataURL(file);
-    }
-</script>
-*/
